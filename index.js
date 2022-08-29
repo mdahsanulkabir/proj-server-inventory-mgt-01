327f3985c00757aa9064317c4e96f7ba84642192
ahsan <makabir98@yahoo.com>
2022-08-29 09:05:27 +0600

    start using mongoose

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -61,6 +63,81 @@ app.get("/ownParts/:id", async (req, res) => {
         const collection = client.db("inventoryDB").collection("ownParts");
         const result = await collection.findOne(query);
         console.log(result);
+        res.send(result);
+    } 
+    catch(error) {
+        console.log(error);
+        throw error;
+    }finally {
+        await client.close();
+    }
+})
+
+app.put("/ownParts/:id", async (req, res) => {
+    console.log('req params :', req.params);
+    const id = req.params.id;
+    const filter = {_id : ObjectId(id)};
+    const options = {upsert : true}
+    const updatePart = req.body;
+    console.log('updated user info :',updatePart);
+    
+    const updatedDoc = {
+        $set: {
+            Unit: updatePart.unit
+        }
+    }
+
+    try{
+        await client.connect();
+        const collection = client.db("inventoryDB").collection("ownParts");
+        const result = await collection.updateOne(filter, updatedDoc, options);
+        console.log('result :',result);
+        res.send(result);
+    } 
+    catch(error) {
+        console.log(error);
+        throw error;
+    }finally {
+        await client.close();
+    }
+})
+
+app.get("/users", async (req, res) => {
+    try{
+        await client.connect();
+        const collection = client.db("inventoryDB").collection("users");
+        const cursor = await collection.find({});
+        const result = await cursor.toArray();
+        // console.log(result);
+        res.send(result);
+    } 
+    catch(error) {
+        console.log(error);
+        throw error;
+    }finally {
+        await client.close();
+    }
+})
+
+app.put("/users/:id", async (req, res) => {
+    console.log('req params :', req.params);
+    const id = req.params.id;
+    const filter = {_id : ObjectId(id)};
+    const options = {upsert : true}
+    const updateUser = req.body;
+    console.log('updated user info :',updateUser);
+
+    const updatedDoc = {
+        $set: {
+            Age: updateUser.userAge
+        }
+    }
+
+    try{
+        await client.connect();
+        const collection = client.db("inventoryDB").collection("users");
+        const result = await collection.updateOne(filter, updatedDoc, options);
+        console.log('result :',result);
         res.send(result);
     } 
     catch(error) {
